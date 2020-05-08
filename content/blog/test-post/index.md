eb3a9461d8a5e3bdd5f49c922045df42b25eed62
Ras Fincher <ras@rasfincher.com>
2020-05-08 13:05:12 -0400

    New post script refined

--- content/blog/test-post/index.md
+++
@@ -1,6 +0,0 @@
----
-title: "test post"
-date: "2020-05-08T14:13:41.541Z"
-description: ""
----
-This is a test post!
--- createPost.js
+++ createPost.js
@@ -1,20 +1,22 @@
-const fs = require("fs")
-const childProcess = require("child_process")
+const fs = require("fs");
+const childProcess = require("child_process");
+const prompt = require("prompt-sync")();
 
-const title = process.argv[2]
-var date = new Date()
+var title = prompt("What would you like the title to be? ");
+var description = prompt("What is the description (if any)? ");
+var date = new Date();
 
-const header = `---\ntitle: "${title}"\ndate: "${date.toISOString()}"\ndescription: ""\n---`
-const dirName = title.replace(" ", "-")
-const path = `./content/blog/${dirName}`
+const header = `---\ntitle: "${title}"\ndate: "${date.toISOString()}"\ndescription: ""\n---`;
+const dirName = title.split(" ").join("-");
+const path = `./content/blog/${dirName}`;
 
-fs.mkdirSync(path)
-fs.writeFileSync(`${path}/index.md`, header)
+fs.mkdirSync(path);
+fs.writeFileSync(`${path}/index.md`, header);
 
 var child = childProcess.spawn("vim", [`${path}/index.md`], {
   stdio: "inherit",
-})
+});
 
 child.on("exit", function(e, code) {
-  console.log("finished!")
-})
+  console.log("finished!");
+});
